cff1156facfe959ae8d9aa560e9f65ba2f30196f
Kyle Mantesso <kylemantesso@pop-os.taile6900f.ts.net>
2026-02-17 14:56:18 +1100

    Fix: add missing clsx and tailwind-merge dependencies

--- packages/ui/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from './button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
-import { Input } from './input';
-import { Label } from './label';
-
-export { Button };
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
-export { Input };
-export { Label };
